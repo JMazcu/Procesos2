d2cf66604eb52b78c69169bcd6adb7219000e9f7
JMazcu <juan.mazcunan@alu.uclm.es>
2025-01-19 21:09:50 +0100

    Implementación del backend

---
+++ servidor/modeloSpec.js
@@ -0,0 +1,37 @@
+const modelo = require("./modelo.js");
+
+describe('El sistema', function () {
+    let sistema;
+
+    beforeEach(function () {
+        sistema = new modelo.Sistema()
+    });
+
+    it('inicialmente no tiene usuarios', function () {
+        expect(sistema.numeroUsuarios()).toEqual(0);
+    });
+
+    it('permite agregar usuarios', function () {
+        sistema.agregarUsuario("u1");
+        expect(sistema.numeroUsuarios()).toEqual(1);
+    });
+
+    it('entrega la lista de usuarios', function () {
+        sistema.agregarUsuario("u1");
+        var usuarios = sistema.obtenerUsuarios();
+        expect(usuarios["u1"].nombre).toEqual("u1");
+    });
+
+    it('permite conocer el estado de los usuarios', function () {
+        sistema.agregarUsuario("u1");
+        var res = sistema.usuarioActivo("u1");
+        expect(res).toEqual("El usuario u1 existe.");
+    });
+
+    it('permite eliminar usuarios', function () {
+        sistema.agregarUsuario("u1");
+        sistema.eliminarUsuario("u1");
+        expect(sistema.numeroUsuarios()).toEqual(0);
+    });
+})
+
