30cbf3a0d66d4ff34d51d839fa1dc8f54241cdd2
JMazcu <juan.mazcunan@alu.uclm.es>
2025-01-19 20:00:14 +0100

    Estructura inicial y pruebas locales

---
+++ cliente/modelo.js
@@ -0,0 +1,34 @@
+function Sistema() {
+
+    this.usuarios = {};
+
+    this.agregarUsuario = function (nombre) {
+        this.usuarios[nombre] = new Usuario(nombre);
+    }
+
+    this.obtenerUsuarios = function () {
+        return this.usuarios;
+    }
+
+    this.usuarioActivo = function (nombre) {
+        if (this.usuarios[nombre]) {
+            return "El usuario " + nombre + " existe.";
+        }
+        return "El usuario " + nombre + " NO existe.";
+    }
+
+    this.eliminarUsuario = function (nombre) {
+        if (this.usuarios[nombre]) {
+            delete this.usuarios[nombre];
+            return "Se ha eliminado al usuario " + nombre;
+        }
+    }
+
+    this.numeroUsuarios = function () {
+        return Object.keys(this.usuarios).length;
+    }
+}
+
+function Usuario(nombre) {
+    this.nombre = nombre;
+}
